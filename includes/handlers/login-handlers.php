79a4046cd23747b33cf44dd8a7dc439280133b09
aqueefcs <53555185+aqueefcs@users.noreply.github.com>
2020-01-12 15:39:59 +0530

    Update login-handlers.php

--- includes/handlers/login-handlers.php
+++ includes/handlers/login-handlers.php
@@ -6,14 +6,16 @@
 
 		$loginQuery = "SELECT * FROM userlogin WHERE uemail = '$uemail' AND upassword = '$upass' "; //select from the existing database
 		$success = mysqli_query($con, $loginQuery);
-		if(mysqli_num_rows($success) != 1 )
+		if(mysqli_num_rows($success) != 1 ){
 			$errorArray['invalidLogin'] = "Login not Authorised";
-		else
+		}
+		else{
 			$row = mysqli_fetch_array($success);//fetching the column of database table and storing it in array.
 			//echo "Hi ".$row[1];//printing the second element in the array
 			$_SESSION['currentUserId'] = $row[0];
 			$_SESSION['userLoggedIn'] = True;
 			header('Location:includes/home.php');
+		}
 	}
 
 	//session variables are the array which will run and store until the programs run.
