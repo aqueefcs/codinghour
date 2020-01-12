c2a7c7bb3e2a51caa9a8fef3035809c872f460e2
aqueefcs <53555185+aqueefcs@users.noreply.github.com>
2020-01-12 13:27:39 +0530

    Update home.php

--- includes/home.php
+++ includes/home.php
@@ -16,6 +16,8 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>HomePage</title>
 	<!-- Font Awesome -->
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
